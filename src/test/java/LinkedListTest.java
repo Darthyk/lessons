09cf65433e743c9582d2505607e8698915aa064c
darthyk <vsazhin@wiley,com>
2021-11-18 16:57:24 +0300

    Revert "Prepare for check"
    This reverts commit 4a79c329

--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -1,4 +1,3 @@
-/*
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -286,4 +285,4 @@ public class LinkedListTest {
         linkedList.addInTail(new LinkedList.Node(4));
         return linkedList;
     }
-}*/
+}
