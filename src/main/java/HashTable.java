47a34eadb8f42b66d86d11b3c8c3ca86ab600682
darthyk <vsazhin@wiley,com>
2022-01-11 19:20:32 +0300

    HashTable

---
+++ src/main/java/HashTable.java
@@ -0,0 +1,69 @@
+public class HashTable
+{
+    public int size;
+    public int step;
+    public String [] slots;
+
+    public HashTable(int sz, int stp)
+    {
+        size = sz;
+        step = stp;
+        slots = new String[size];
+        for(int i=0; i<size; i++) slots[i] = null;
+    }
+
+    public int hashFun(String value)
+    {
+        // всегда возвращает корректный индекс слота
+        return value.length() % size;
+    }
+
+    public int seekSlot(String value)
+    {
+        int hash = hashFun(value);
+        for (int i = 0; i < size; i++) {
+            if (slots[hash] == null) {
+                return hash;
+            } else {
+                if (slots[hash].equals(value)) {
+                    return -1;
+                }
+                hash += step;
+            }
+        }
+        // находит индекс пустого слота для значения, или -1
+        return -1;
+    }
+
+    public int put(String value)
+    {
+        int slot = seekSlot(value);
+        if (slot == -1) {
+            return -1;
+        } else {
+            slots[slot] = value;
+            return slot;
+        }
+        // записываем значение по хэш-функции
+
+        // возвращается индекс слота или -1
+        // если из-за коллизий элемент не удаётся разместить
+    }
+
+    public int find(String value)
+    {
+        int hash = hashFun(value);
+        for (int i = 0; i < (size / step); i++) {
+            if (slots[hash] != null) {
+                if (slots[hash].equals(value)) {
+                    return hash;
+                }
+            } else {
+                hash += step;
+            }
+        }
+
+        // находит индекс слота со значением, или -1
+        return -1;
+    }
+}
---
+++ src/test/java/HashTableTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HashTableTest {
+    @Test
+    public void test() {
+        HashTable table = new HashTable(19, 3);
+        assertEquals(4 % 19, table.put("aaaa"));
+        assertEquals(-1, table.put("aaaa"));
+        assertEquals(5 % 19, table.put("aaabf"));
+        assertEquals(2 % 19, table.put("aa"));
+        assertEquals(4 % 19, table.find("aaaa"));
+        assertEquals(4 % 19 + 3, table.seekSlot("avby"));
+    }
+}
