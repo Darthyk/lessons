c96cef337f7e3ea2e6afd4d31f26a8ba575206ce
darthyk <vsazhin@wiley,com>
2022-01-19 16:54:38 +0300

    BloomFilter

---
+++ src/test/java/BloomFilterTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BloomFilterTest {
+    @Test
+    public void tes() {
+        BloomFilter bloomFilter = new BloomFilter(32);
+        bloomFilter.add("0123456789");
+        bloomFilter.add("2345678901");
+        bloomFilter.add("7890123456");
+        assertTrue(bloomFilter.isValue("0123456789"));
+        assertTrue(bloomFilter.isValue("2345678901"));
+        assertTrue(bloomFilter.isValue("7890123456"));
+        assertFalse(bloomFilter.isValue("qwerty"));
+    }
+}
