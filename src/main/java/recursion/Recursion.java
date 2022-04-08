d5bad1e42d11dfa8ecaba61f075c45f5696baa78
darthyk <vsazhin@wiley,com>
2022-04-08 23:14:29 +0300

    Add array length calculation by recursion and check that string is palindrome

--- src/main/java/recursion/Recursion.java
+++ src/main/java/recursion/Recursion.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import java.util.List;
+
 public class Recursion {
     public static int doExponentiation(int n, int m) {
         int result = 1;
@@ -15,5 +17,22 @@ public class Recursion {
             result += number % 10 + numberSum(number / 10);
         }
         return result;
+    }
+
+    public static int arrayLength(List<Integer> array) {
+        if (array.isEmpty()) {
+            return 0;
+        } else {
+            array.remove(0);
+            return 1 + arrayLength(array);
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        if (str.length() <= 1) {
+            return true;
+        } else {
+            return str.charAt(0) == str.charAt(str.length() - 1) && isPalindrome(str.substring(1, str.length() - 1));
+        }
     }
 }
--- src/test/java/RecursionTest.java
+++ src/test/java/RecursionTest.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import recursion.Recursion;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RecursionTest {
 
@@ -18,5 +23,32 @@ public class RecursionTest {
         assertEquals(6, Recursion.numberSum(111111));
         assertEquals(14, Recursion.numberSum(2345));
         assertEquals(1, Recursion.numberSum(1));
+    }
+
+    @Test
+    public void testArrayLengthCalculation() {
+        assertEquals(0, Recursion.arrayLength(generateList(0)));
+        assertEquals(1, Recursion.arrayLength(generateList(1)));
+        assertEquals(100, Recursion.arrayLength(generateList(100)));
+        assertEquals(1000, Recursion.arrayLength(generateList(1000)));
+    }
+
+    @Test
+    public void testIsStringPalindrome() {
+        assertTrue(Recursion.isPalindrome("ababa"));
+        assertTrue(Recursion.isPalindrome("avavava"));
+        assertTrue(Recursion.isPalindrome("abba"));
+
+        assertTrue(Recursion.isPalindrome("aa"));
+        assertTrue(Recursion.isPalindrome("a"));
+        assertFalse(Recursion.isPalindrome("avav"));
+    }
+
+    private List<Integer> generateList(int length) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            list.add(i);
+        }
+        return list;
     }
 }
